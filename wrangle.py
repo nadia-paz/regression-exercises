d1acbc5eccaaa1381e2cc04a26372897839a47a5
Nadia Paz <nadina.abouhamdan@gmail.com>
2022-11-07 14:48:03 -0600

    add split and scale functions

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,13 @@
 import os
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.preprocessing import QuantileTransformer, PowerTransformer
+
 from env import get_db_url
+
+seed = 2912
 
 def get_zillow():
     '''
@@ -36,5 +43,42 @@ def get_zillow():
     df['fips'] = df.loc[:, 'fips'].astype(int)
     #remove null values   
     df = df.dropna()
+
+    #dropped very small and unrealistic square feet
+    df = df[df.calculatedfinishedsquarefeet > 100]
     
     return df 
+
+def split_zillow(df):
+    '''
+    This function takes in a dataframe and splits it into 3 data sets
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    #split_db class verision with random seed
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed)
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed)
+    return train, validate, test
+
+def scale_zillow(train, validate, test):
+    '''
+    accepts train, validate, test data sets
+    scales the data in each of them
+    returns transformed data sets
+    '''
+    #count_columns = ['bedroomcnt', 'bathroomcnt']
+    
+    col = train.columns[1:-1]
+    
+    # create scalers
+    #min_max_scaler = MinMaxScaler()    
+    qt = QuantileTransformer(output_distribution='normal')
+    qt.fit(train[col])
+    train[col] = qt.transform(train[col])
+    validate[col] = qt.transform(validate[col])
+    test[col] = qt.transform(test[col])
+    
+    return train, validate, test
